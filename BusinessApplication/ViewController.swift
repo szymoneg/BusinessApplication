82ff815e8b88eea0d8c2e2f6bede1889c5a06a3a
szymoneg <szymon.zsp5@gmail.com>
2021-03-22 23:37:40 +0100

    init

---
+++ BusinessApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessApplication
+//
+//  Created by Szymon Biliński on 22/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
